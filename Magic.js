ded37985e957858f49aa58f04a666516be678f3e
TH4LY5 <thalyscosta99@gmail.com>
2022-09-07 11:46:58 -0300

    animacao em css

---
+++ Magic.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $surface = $('.surface');
+    $car = $('.car');
+
+    $(document).on('keypress', function(e){
+        if(e.which == 13){
+            $($surface).toggleClass('moveRight')
+        }
+    })
+})
